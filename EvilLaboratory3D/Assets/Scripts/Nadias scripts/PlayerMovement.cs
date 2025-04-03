fbda30c9776cab96832ff5810071ea201ba53c40
Nadia Yakta <nadia.yakta@learnet.se>
2025-04-03 10:47:44 +0200

    idk

--- EvilLaboratory3D/Assets/Scripts/Nadias scripts/PlayerMovement.cs
+++ EvilLaboratory3D/Assets/Scripts/Nadias scripts/PlayerMovement.cs
@@ -2,28 +2,63 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 5f;
+    public float sprintSpeed = 8f;
+    private float currentSpeed;
 
-    [SerializeField] Rigidbody rb;
+    [Header("Mouse Look")]
+    public float mouseSensitivity = 100f;
+    public Transform playerCamera;
+    private float xRotation = 0f;
+
+    [Header("Jumping")]
+    public float jumpForce = 5f;
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        currentSpeed = moveSpeed;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Mouse Look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
-        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        if (movement != Vector3.zero)
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        rb.linearVelocity = new Vector3(move.x * currentSpeed, rb.linearVelocity.y, move.z * currentSpeed);
+
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = sprintSpeed;
+        else
+            currentSpeed = moveSpeed;
+
+        // Jump
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            transform.forward = movement;
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
         }
-
-        rb.linearVelocity = movement;
     }
 }
