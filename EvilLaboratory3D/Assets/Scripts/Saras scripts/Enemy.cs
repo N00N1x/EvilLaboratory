d196783cf0b9846b70b103eff86606927f2eca63
Sara <sara.guellil@elev.ga.lbs.se>
2025-04-24 08:43:24 +0200

    .

--- EvilLaboratory3D/Assets/Scripts/Saras scripts/Enemy.cs
+++ EvilLaboratory3D/Assets/Scripts/Saras scripts/Enemy.cs
@@ -4,55 +4,21 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    GameObject player;
-
     NavMeshAgent agent;
+    PlayerMovement player;
 
-    [SerializeField] LayerMask groundLayer, playerLayer;
-
-    // Patrol
-    Vector3 destPoint;
-    bool WalkpointSet;
-    [SerializeField] float range;
-
-    //state change
-    [SerializeField] float sightRange, attackRange;
-    bool playerInSight, playerinAttackRange;
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        player = GameObject.Find("Player");
+        player = FindFirstObjectByType<PlayerMovement>();
     }
 
     void Update()
     {
-        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
-        playerinAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
-
-
-        if(!playerInSight && !playerinAttackRange) Patrol();
-        if (playerInSight && !playerinAttackRange) Chase();
-        if (playerInSight && playerinAttackRange) Attack();
-    }
-
-    void Chase()
-    {
         agent.SetDestination(player.transform.position);
-    }
-
-    void Attack()
-    {
-        
-    }
-
-    void Patrol()
-    {
-
-    }
-
-    void SearchForDest()
-    {
-
     }
 }
