c66c82b8b17f4a77f6fe9ff3dc1029bbcd42864c
Sara <saraguellil2007@gmail.com>
2025-05-15 13:34:13 +0200

    Main Menu, Game Over Menu

--- EvilLaboratory3D/Assets/Scripts/Hugos scripts/Health.cs
+++ EvilLaboratory3D/Assets/Scripts/Hugos scripts/Health.cs
@@ -5,6 +5,8 @@ public class Health : MonoBehaviour
 {
     [SerializeField] public int maxHealth = 100;
     [SerializeField] public int currentHealth;
+    public GameObject gameOverUI;
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -12,7 +14,10 @@ public class Health : MonoBehaviour
     private void Die()
     {
         Debug.Log("Player has died");
-        //SceneManager.LoadScene("NadiasWork");
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(true);
+        }
     }
 
     public void Heal(int amount)
@@ -40,9 +45,5 @@ public class Health : MonoBehaviour
             Die();
         }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
