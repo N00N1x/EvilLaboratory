9d43be54f587de33dc11b1ce70bcfb6aa47772fa
Johnny Ung <johnny.ung@elev.ga.lbs.se>
2025-05-22 09:09:26 +0200

    small stuff to my code
    removed firerate and added enemydamage to the melee section

--- EvilLaboratory3D/Assets/Scripts/Johnnys scripts/Weapon.cs
+++ EvilLaboratory3D/Assets/Scripts/Johnnys scripts/Weapon.cs
@@ -4,8 +4,6 @@ public class Weapon : MonoBehaviour
 {
     public int ShootDamage = 1;
     public int MeleeDamage = 1/2;
-    public float FireRate = 0.5f;
-    public float MeleeRate = 1f;
     public Camera fpsCam;
     public float Shootrange = 100f;
     public float Meleerange = 1.5f;
@@ -47,7 +45,11 @@ public class Weapon : MonoBehaviour
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, Meleerange))
         {
             Debug.Log("Hit: " + hitInfo.transform.name);
-            // Additional hit effects can be implemented here
+            EnemyHealth enemy = hitInfo.transform.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(ShootDamage);
+            }
         }
     }
 }
